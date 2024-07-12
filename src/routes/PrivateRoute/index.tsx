b4915ec89c887a236525c2af4fdb2d9baf786009
hwangheetae <tejaia@naver.com>
2024-07-13 00:18:51 +0900

    feat: 토스트 메시지 추가

--- src/routes/PrivateRoute/index.tsx
+++ src/routes/PrivateRoute/index.tsx
@@ -2,8 +2,24 @@
 import { Navigate } from 'react-router-dom';
 import useAuthStore from '../../stores/authStore';
 import { useToast } from '@chakra-ui/react';
+import { useEffect } from 'react';
+
 const PrivateRoute = ({ children }: { children: JSX.Element }) => {
   const user = useAuthStore((state) => state.user);
+  const toast = useToast();
+
+  useEffect(() => {
+    if (!user) {
+      toast({
+        title: '로그인 필요',
+        description: '로그인을 해주세요!',
+        status: 'warning',
+        position: 'top-right',
+        isClosable: true,
+        duration: 5000,
+      });
+    }
+  }, [user, toast]);
 
   if (!user) {
     return <Navigate to="/login" />;
