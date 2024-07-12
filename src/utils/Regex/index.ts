093d6e9c28a9e07339ea8543e66114faa8c67ec8
hwangheetae <tejaia@naver.com>
2024-07-12 18:20:42 +0900

    feat: 회원가입 UI (미완)

---
+++ src/utils/Regex/index.ts
@@ -0,0 +1,4 @@
+export const emailRegEx =
+  /^[A-Za-z0-9]([._-]?[A-Za-z0-9]+)*@[A-Za-z0-9]([._-]?[A-Za-z0-9]+)*\.[A-Za-z]{2,}$/i;
+export const passwordRegEx =
+  /^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d!@#$%^&*?_]{8,20}$/;
