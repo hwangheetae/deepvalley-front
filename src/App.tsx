f4f02c823a7b4f0ddd0bc5d8295ccbb28dc23243
jaebin <dlwoqls4280@ajou.ac.kr>
2024-07-15 10:54:59 +0900

    test. dev와 merge전 App.tsx main.tsx수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,10 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ChakraProvider } from '@chakra-ui/react';
-import Review from './components/Common/Review/Review';
-import { fetchReview } from './api/ReviewApi/ReviewApi';
-import { mockReview } from './api/ReviewApi/MockData'; //테스트용
-
-const queryClient = new QueryClient();
-
-const router = createBrowserRouter([
-  {
-    path: 'review/:reviewId',
-    element: <Review />,
-    loader: async ({ params }) => {
-      const reviewId = params.reviewId as string;
-      const data = await fetchReview(reviewId);
-      queryClient.setQueryData(['reviewType', reviewId], data);
-      return data;
-    }
-  }
-]);
-
-const App: React.FC = () => {
-  queryClient.setQueryData(['reviewType', 1], mockReview); //테스트용
+import { Outlet } from 'react-router-dom';
+function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ChakraProvider>
-        <RouterProvider router={router} />
-      </ChakraProvider>
-    </QueryClientProvider>
+    <div>
+      <Outlet />
+    </div>
   );
-};
+}
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,70 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.tsx';
+import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import HomePage from './pages/HomePage';
+import TestPage from './TestPage.tsx';
+import ImagePage from './pages/ImagePage';
+import Review from './components/Common/Review/Review.tsx';
+import { fetchReview } from './api/ReviewApi/ReviewApi.ts';
+import { mockReview } from './api/ReviewApi/MockData.ts'; //테스트용
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
+const breakpoints = {
+  sm: '320px',
+  md: '768px',
+  lg: '960px',
+  xl: '1200px',
+  '2xl': '1536px',
+};
+
+const theme = extendTheme({ breakpoints });
+
+const queryClient = new QueryClient();
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <HomePage />,
+      },
+      {
+        path: '/test',
+        element: <TestPage />,
+        path: 'imagepage',
+        element: <ImagePage />,
+      },
+      {
+        path: 'review/:reviewId',
+        element: <Review />,
+        loader: async({ params }) => {
+          const reviewId = params.reviewId as string;
+          queryClient.setQueryData(['reviewType', 1], mockReview); //테스트용
+          // const data = await fetchReview(reviewId);
+          // queryClient.setQueryData(['reviewType', reviewId],data);
+          // return data;
+        }
+      }
+    ],
+    //EX: loader, errorElement 예시
+    // 데이터 패칭 및 로더 가능
+    // loader: rootLoader,
+
+    //에러처리 페이지 ex)404
+    // errorElement: <ErrorPage />
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+    <ChakraProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </ChakraProvider>
+  </React.StrictMode>,
+);
