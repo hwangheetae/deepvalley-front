24e7ab845e18d122532dec2ef3179f15623b0260
hwangheetae <tejaia@naver.com>
2024-07-19 17:47:58 +0900

    feat: api key update

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -1,5 +1,8 @@
 const config = {
   API_URL: import.meta.env.VITE_API_URL,
+  KAKAO_REST_API_KEY: import.meta.env.VITE_KAKAO_REST_API_KEY,
+  KAKAO_REDIRECT_URL: import.meta.env.VITE_KAKAO_REDIRECT_URL,
+  KAKAO_MAP_KEY: import.meta.env.VITE_KAKAOMAP_KEY,
 };
 
 export default config;
