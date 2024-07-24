fe8660a6ff5699a39774b87a4ec586f76f8db1ee
hwangheetae <tejaia@naver.com>
2024-07-24 12:02:02 +0900

    fix: build warning error

--- src/api/Review/index.ts
+++ src/api/Review/index.ts
@@ -1,5 +1,5 @@
 // 해당부분이 User 03-a 맞나요?? 03-a 기준 재작성해놓았는데 확인부탁드립니다.
-import basicClient from '../Auth/basicClient';
+// import basicClient from '../Auth/basicClient';
 // memberId: string
 // export const fetchReviews = async (body: {}) => {
 //   return basicClient.get('api/member/${memberId}/review', body);
